e2b2a5f8458c5d5e9966bc1deadb1860a9946540
Marcos Vigorelli <vigorellimarcos@gmail.com>
2022-01-11 20:09:53 -0300

    segundo commit

---
+++ js/java.js
@@ -0,0 +1,9 @@
+window.onscroll = function(){ 
+    if(document.documentElement.scrollTop > 100){
+        document.querySelector(".ir-top-container").classList.add("show");
+    }else{(document.documentElement.scrollTop > 100){
+        document.querySelector(".ir-top-container").classList.remove("show");
+
+    }
+
+} 
