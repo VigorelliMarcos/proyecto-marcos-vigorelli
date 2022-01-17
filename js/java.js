392dffe64196f568a2864472a6143d621f330d10
Marcos Vigorelli <vigorellimarcos@gmail.com>
2022-01-17 00:04:40 -0300

    commit5-animaciones2

--- js/java.js
+++ js/java.js
@@ -3,7 +3,7 @@ caja.addEventListener("click",function(){
     document.documentElement.scrollTop=0;
 })
 window.addEventListener("scroll",function(){
-    if (document.documentElement.scrollTop > 0) {
+    if (document.documentElement.scrollTop > 300) {
         caja.style.display="flex"
     } else {
         caja.style.display="none"
